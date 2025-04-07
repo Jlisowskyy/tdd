884a4e6a437f4b0834dc2e933e814e3aaf8314aa
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-04-07 09:21:21 +0200

    Added factory

--- src/io_lib.hpp
+++ src/io_lib.hpp
@@ -3,28 +3,82 @@
 
 #include <string>
 #include <exception>
+#include <memory>
+#include <unordered_map>
+#include <functional>
 
-class ArgumentException : public std::exception {
+class ArgumentException final : public std::exception {
     std::string message;
-public:
-    explicit ArgumentException(const std::string& msg)
-        : message("ArgumentException: " + msg) {}
 
-    const char* what() const noexcept override {
+public:
+    explicit ArgumentException(const std::string &msg)
+        : message("ArgumentException: " + msg) {
+    }
+
+    const char *what() const noexcept override {
         return message.c_str();
     }
 };
+
 class IMachine {
 public:
     IMachine() = default;
+
     virtual ~IMachine() = default;
 
     virtual double GetPowerConsumption(int duration, bool is_energy_saving) = 0;
 };
 
-class MachinePowerCalculator {
+class MillingMachine : public IMachine {
+    static constexpr double kBaseConsumption = 5.0; // Base power consumption in kWh
 public:
-    double GetPowerConsumption(const std::string& machine_type, int duration, bool is_energy_saving = false) {
+    MillingMachine() = default;
+
+    ~MillingMachine() override = default;
+
+    double GetPowerConsumption(int duration, [[maybe_unused]] bool is_energy_saving) override {
+        return kBaseConsumption * duration;
+    }
+};
+
+class IMachineFactory {
+public:
+    IMachineFactory() = default;
+
+    virtual ~IMachineFactory() = default;
+
+    virtual std::shared_ptr<IMachine> CreateMachine(const std::string &) = 0;
+};
+
+class MachineFactory : public IMachineFactory {
+public:
+    MachineFactory() = default;
+    ~MachineFactory() override = default;
+
+    std::shared_ptr<IMachine> CreateMachine(const std::string &machine_type) override {
+        static std::unordered_map<std::string, std::function<std::shared_ptr<IMachine>()>> machine_map = {
+            {"MillingMachine", []() { return std::make_shared<MillingMachine>(); }},
+        };
+
+        if (machine_map.find(machine_type) != machine_map.end()) {
+            return machine_map[machine_type]();
+        }
+
+        throw ArgumentException("Unknown machine type: " + machine_type);
+    }
+};
+
+class MachinePowerCalculator {
+    std::shared_ptr<IMachineFactory> factory_;
+
+public:
+    explicit MachinePowerCalculator(std::shared_ptr<IMachineFactory> factory): factory_(std::move(factory)) {
+        if (!factory_) {
+            throw ArgumentException("Factory cannot be null");
+        }
+    }
+
+    double GetPowerConsumption(const std::string &machine_type, int duration, bool is_energy_saving = false) {
         if (machine_type.empty()) {
             throw ArgumentException("Machine type cannot be empty");
         }
